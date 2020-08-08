5cd5ad71002e22cd3f0c2fd9faa5a88f1bd20280
Andrew <anfriy.zhuravlov@gmail.com>
2020-08-08 19:28:57 +0300

    added cart logic

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
-
+import {Link} from 'react-router-dom';
 class Default extends Component {
     render() {
+        console.log(this.props);
         return (
-            <div>
-                <h3>page not found</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+                        <h1 className="display-4">404 <br/>error</h1>
+                        <h2>page not found</h2>
+                        <h3>the requested URL:  <span className="text-danger">{this.props.location.pathname}</span> was not found. <br/>Please return to <Link to="/">Home page</Link></h3>
+                    </div>
+                </div>
             </div>
         );
     }
